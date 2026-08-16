c3fc3cc566a05ca87c9903c05f184438c0c9c12d
spupuz <spupuz@gmail.com>
2026-08-16 22:10:19 +0200

    v0.10.3: merge PR #71
    - Merge PR #71: Register mktemp tmp_file for cleanup in all scripts to prevent resource leaks

--- lxc-updater.sh
+++ lxc-updater.sh
@@ -24,7 +24,7 @@
 
 set -Eeuo pipefail
 
-SCRIPT_VERSION="v0.10.2"
+SCRIPT_VERSION="v0.10.3"
 
 # --- LOGGING ---
 LOG_STDOUT="${LOG_STDOUT:-yes}" # Set to "no" to disable console output (useful for cron)
