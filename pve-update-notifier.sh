ed377a5b647d18d12dbb1635a331fe3632817aba
spupuz <spupuz@gmail.com>
2026-08-18 12:28:48 +0200

    v0.10.7: merge PR #75, add UX mirror logging
    - Merge PR #75: Mirror Background Update Task States to CLI

--- pve-update-notifier.sh
+++ pve-update-notifier.sh
@@ -14,7 +14,7 @@
 # Use this script at your own risk. The authors are not responsible for any
 # data loss, system instability, or service downtime caused by running it.
 
-SCRIPT_VERSION="v0.10.6"
+SCRIPT_VERSION="v0.10.7"
 
 # Add this path variable so Cron can find the required system commands
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
