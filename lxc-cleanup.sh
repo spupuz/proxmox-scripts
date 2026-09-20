9f19ab19a5a0f2827f355d4d3abeedfed001a1eb
Belloni Alessandro <spupuz@gmail.com>
2026-09-21 00:44:46 +0200

    v0.17.2: merge PR #139, bash pct list parsing
    - Merge PR #139: Replace awk with pure Bash parsing for pct list

--- lxc-cleanup.sh
+++ lxc-cleanup.sh
@@ -30,7 +30,7 @@
 
 set -Eeuo pipefail
 
-SCRIPT_VERSION="v0.17.1"
+SCRIPT_VERSION="v0.17.2"
 
 # --- LOGGING ---
 LOG_STDOUT="${LOG_STDOUT:-yes}" # Set to "no" to disable console output (useful for cron)
